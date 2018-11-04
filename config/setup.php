ffc0333c44d637356e6f0e4a3d692aafeb0e3c8e
Valerie <mhizhagongora@gmail.com>
2018-11-04 23:30:25 +0200

    first push

---
+++ config/setup.php
@@ -0,0 +1,29 @@
+<?php
+include "database.php";
+
+try{
+ $conn = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ $sql = "CREATE DATABASE IF NOT EXISTS vgongora";
+ $conn->exec($sql);
+}
+catch (PDOException $e){
+	echo "Error Creating Database: ".$e->getMessage();
+}
+$conn = null;
+
+//Create User Table
+try{
+ $conn = new PDO($DB_DSNM, $DB_USER, $DB_PASSWORD);
+ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ $sql = "CREATE TABLE IF NOT EXISTS users(
+	 `id` INT(100) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	 `username` VARCHAR(100) NOT NULL,
+	 `email` VARCHAR(100) NOT NULL,
+	 `password` VARCHAR(100) NOT NULL)";
+ $conn->exec($sql);
+}
+catch (PDOException $e){
+	echo "Error Creating Tables: ".$e->getMessage();
+}
+?>
---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Login</title>
+		<link rel="stylesheet" href="css/login.css">
+		<body>
+            <div class="loginbox">
+                <h1>Login</h1>
+                <div class="textbox">
+                    <input type="text" placeholder="Username"  name="" value="">
+                </div>
+                <div class="textbox">
+                    <input type="password" placeholder="Password" name="" value="">
+                </div>
+                <input class="button" type="button" name="" value="Sign In">
+            </div>
+        </body>
+</html>
